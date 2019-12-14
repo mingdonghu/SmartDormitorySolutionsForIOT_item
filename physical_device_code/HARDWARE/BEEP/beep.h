bd90e376d000360f9fcc875c2d07e189a9545b93
JCxiaohugong <13129931623@163.com>
2019-12-14 21:13:01 +0800

    更新了 物理终端设备开发计划与日志.txt

---
+++ physical_device_code/HARDWARE/BEEP/beep.h
@@ -0,0 +1,15 @@
+#ifndef __BEEP_H
+#define __BEEP_H
+
+#include "sys.h"
+
+//beep port defines
+#define BEEP PBout(8)
+
+//beep function declare
+void BEEP_init(void);
+
+
+
+#endif
+
--- physical_device_code/SI/Backup/main(5553).c
+++ physical_device_code/SI/Backup/main(5553).c
@@ -9,22 +9,24 @@ void UART_test(void)
 {
 	u16 t;  
 	u16 len;
-
+	
 	if(USART_RX_STA&0x8000){					   
-		len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
-		printf("\r\n您发送的消息为:\r\n\r\n");
-		for(t=0;t<len;t++){
-			USART_SendData(USART1, USART_RX_BUF[t]);//向串口1发送数据
-			while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);//等待发送结束
-		}
-		printf("\r\n\r\n");//插入换行
-		USART_RX_STA=0;
+			len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
+			printf("\r\n您发送的消息为:\r\n\r\n");
+			for(t=0;t<len;t++){
+				USART_SendData(USART1, USART_RX_BUF[t]);//向串口1发送数据
+				while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);//等待发送结束
+			}
+			printf("\r\n\r\n");//插入换行
+			USART_RX_STA=0;
 			
 	}else{
-		printf("请输入数据,以回车键结束\n");
-		LED1=!LED1;//闪烁LED1[green],提示系统正在运行.
-		delay_ms(100); 	
+			printf("请输入数据,以回车键结束\n");
+			LED1=!LED1;//闪烁LED1[green],提示系统正在运行.
+			delay_ms(100); 
+			
 	}
+	
 }
 
 
@@ -46,6 +48,8 @@ while(1)
 	delay_ms(300);
 	LED1 = !LED1;
 	delay_ms(300);
+
+
 		
 }	
 	
